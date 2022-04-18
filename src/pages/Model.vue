c259cefb3eae9fe3407477f676eb696ceb5814f8
Camille <camille.coutens@gmail.com>
2022-04-18 14:43:02 +0200

    add horse model

---
+++ src/pages/Model.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {  Camera,  AmbientLight, Renderer, Scene, GltfModel } from 'troisjs';
+
+export default defineComponent({
+  name: 'Model',
+  components: {  Camera,  AmbientLight, Renderer, Scene, GltfModel },
+})
+</script>
+<template>
+  <div class="container">
+    <Renderer resize="window" orbit-ctrl ref="renderer">
+      <Camera :position="{ z: 1 }" />
+      <Scene background="#fff">
+        <AmbientLight :intensity="4" />
+        <GltfModel
+          src="/models/horse/horse.gltf"
+        />
+      </Scene>
+    </Renderer>
+  </div>
+</template>
